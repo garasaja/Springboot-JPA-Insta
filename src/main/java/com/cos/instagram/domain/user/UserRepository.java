2fb9c09dd9ba2225d0bfd4f843971e2999ddddba
kdm <cokj1610@naver.com>
2020-08-25 10:48:31 +0900

    7. 회원가입 완료(email필드추가, username unique제약조건 추가)

--- src/main/java/com/cos/instagram/domain/user/UserRepository.java
+++ src/main/java/com/cos/instagram/domain/user/UserRepository.java
@@ -2,7 +2,12 @@ package com.cos.instagram.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.cos.instagram.web.dto.JoinReqDto;
+
 // JpaRepository가 extends가 되면 @Repository가 필요없음 ioc 자동으로 됨
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+	
+	
+	
 }
